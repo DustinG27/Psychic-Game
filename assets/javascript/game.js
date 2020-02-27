b1f300ce4c6359d1e28436065e4cb392dda166d5
dustin george <DustinG27@gmail.com>
2020-02-26 21:14:57 -0500

    game will keep track of user guesses up to 9 spaces

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -31,7 +31,6 @@ var computerChoices = [
 // variables to hold the number of wins, loses, guesses, and guess attempts
 var wins = 0;
 var losses = 0;
-var counter = 0;
 var guessedLetters = [];
 var guessesLeft = 9;
 var secretLetter = [];
@@ -39,7 +38,7 @@ var secretLetter = [];
 // Create variables that hold references to the places in the HTML where we want to display things.
 
 var winsText = document.getElementById("wins-text");
-var userChoiceText = document.getElementById("user-Guess");
+
 var lossesText = document.getElementById("losses-text");
 var guessesText = document.getElementById("guess-left");
 
@@ -54,61 +53,52 @@ function getRandomLetter() {
 }
 console.log(secretLetter);
 
+// functions that will keep track of user guesses and guesses left
+
+function trackGuessesLeft() {
+  document.querySelector("#guess-left").innerHTML =
+    "Guesses left: " + guessesLeft;
+}
+
+function trackUserGuess() {
+  document.querySelector("#user-Guess").innerHTML =
+    "You guessed: " + guessedLetters.join(", ");
+}
+
+// reset function to exicute when if statement is completed
+
+var reset = function() {
+  guessedLetters = [];
+  guessesLeft = 9;
+  secretLetter = [];
+};
+
 // when user clicks they will log their guess vs the secretLetter
 
 document.onkeyup = function(event) {
-  var userGuess = event.key;
+  // start of the game on key up
+
+  var userGuess = event.key.toLowerCase();
+
+  guessesLeft--;
+
+  // runs the function to keep track of user guesses
+
+  guessedLetters.push(userGuess);
+  trackUserGuess();
+  trackGuessesLeft();
+
+  // where the game will compare user to computer
 
   if (userGuess === secretLetter) {
     wins++;
+    winsText.textContent = "wins: " + wins;
+    reset();
     console.log("you win " + wins);
-  } else {
-    guessedLetters++;
-    guessesLeft--;
-    console.log(guessesLeft);
-    if (guessesLeft === 0) {
-      losses++;
-      guessesLeft = 9;
-      console.log("you lose " + losses);
-    }
-  }
-
-  userChoiceText.textContent = "You guessed " + userGuess;
-  winsText.textContent = "wins: " + wins;
-  guessesText.textContent = "Guesses left: " + guessesLeft;
-  lossesText.textContent = "Losses: " + losses;
-};
-/*  var secretLetter = getRandomLetter();
-function getRandomLetter() {
-  var random =
-    computerChoices[Math.floor(Math.random() * computerChoices.length)];
-  return random;
-}
-console.log(secretLetter);
-
-//  starting the game with user input
-document.onkeyup = function(event) {
-  userGuess = event.key;
-  // max tries will go down to 0, when it does it restarts
-  Maxtries--;
-  console.log(Maxtries);
-  // testing user guess to the secret letter
-  if (userGuess === secretLetter) {
-    wins++;
-    Maxtries = 9;
-    console.log("You win! " + wins);
-  } else if (Maxtries === 0) {
-    console.log("You lose " + loses);
-    Maxtries = 9;
-    loses++;
-    guessedLetters.length = 0;
-    console.log(loses);
-    console.log(guessedLetters);
+  } else if (guessesLeft === 0) {
+    losses++;
+    lossesText.textContent = "Losses: " + losses;
+    reset();
+    console.log("you lose " + losses);
   }
 };
-
-winsText.textContent = "wins " + wins;
-userGuess.textContent = "You guessed " + guessedLetters;
-lossesText.textContent = "Lose: " + loses;
-
-*/
